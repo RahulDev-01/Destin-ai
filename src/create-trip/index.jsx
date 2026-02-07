558d8aadd89e6efb322a9a7ae35edc6d391c6087
S.Rahul <s.rahul5116@outlook.com>
2026-02-07 08:28:54 +0530

    Upadeted Index.jsx

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -370,7 +370,7 @@ function CreateTrip() {
       <div className="min-h-screen bg-indigo-50 py-12 px-4 sm:px-6 lg:px-8 relative overflow-hidden">
 
         {/* Decorative Background Blobs */}
-        <div className="absolute -top-24 -left-24 w-96 h-96 bg-purple-400 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob"></div>
+        <div className="absolute -top-24 -left-24 w-96 h-96 bg-blue-400 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob"></div>
         <div className="absolute -top-24 -right-24 w-96 h-96 bg-blue-400 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-2000"></div>
         <div className="absolute -bottom-24 left-1/2 -translate-x-1/2 w-96 h-96 bg-pink-400 rounded-full mix-blend-multiply filter blur-3xl opacity-20 animate-blob animation-delay-4000"></div>
 
